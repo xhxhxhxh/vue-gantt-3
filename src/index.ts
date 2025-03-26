4c3ca96d0b6490ec34ec827d921f51544dd81f33
XH <546151373@qq.com>
2025-03-26 18:47:43 +0800

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Gantt from './Gantt.vue';
 import { withInstall } from '@/utils/withInstall';
 
+export * from './types';
+
 export const Vue3Gantt = withInstall(Gantt);
 export default Vue3Gantt;
