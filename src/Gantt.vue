c72a9bac6b3fa611b95842a6043eccbc5fdf21f3
XH <546151373@qq.com>
2025-03-27 16:43:01 +0800

    update

--- src/Gantt.vue
+++ src/Gantt.vue
@@ -46,7 +46,8 @@
 <script lang="ts" setup>
 import TableView from "./components/tableView/TableView.vue";
 import GanttView from "./components/ganttView/GanttView.vue";
-import type { RowData, ColDef, DefaultColDef, GanttRowNode, GanttStyleOption, TimePoint, MovedTimeLineData, TimeScale } from '@/types';
+import type { RowData, DefaultColDef, GanttRowNode, GanttStyleOption, TimePoint, MovedTimeLineData, TimeScale } from '@/types';
+import type { ColDef } from "ag-grid-community";
 import { ref, provide, toRef } from 'vue';
 import ExpandableBox from './components/common/ExpandableBox.vue';
 import dayjs from 'dayjs';
